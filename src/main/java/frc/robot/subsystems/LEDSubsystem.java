ad9eff63b61e326315653140123be71a7ae090b7
857 programming <csdoig@mtu.edu>
2023-02-14 18:23:23 -0500

    Initial subsystem to control LEDs

---
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDSubsystem extends SubsystemBase{
+
+  private Spark ledPWM = new Spark(9);
+
+  public LEDSubsystem() {
+  
+  }
+
+  @Override
+  public void periodic() {
+    
+  }
+
+  public void setBlue() {
+    ledPWM.set(0.87);
+  }
+
+  public void setPurple() {
+    ledPWM.set(0.91);
+  }
+
+  public void setYellow() {
+    ledPWM.set(0.69);
+  }
+}
