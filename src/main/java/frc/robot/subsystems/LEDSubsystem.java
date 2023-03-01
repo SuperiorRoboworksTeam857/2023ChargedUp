5324910fb3842abf545f86e9d05999de1ed822b6
857 programming <csdoig@mtu.edu>
2023-02-28 19:48:22 -0500

    Formatting

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,22 +1,16 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
-
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class LEDSubsystem extends SubsystemBase{
+public class LEDSubsystem extends SubsystemBase {
 
   private Spark ledPWM = new Spark(9);
 
-  public LEDSubsystem() {
-  
-  }
+  public LEDSubsystem() {}
 
   @Override
-  public void periodic() {
-    
-  }
+  public void periodic() {}
 
   public void setBlue() {
     ledPWM.set(0.87);
