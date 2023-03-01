e1e06fd065d53ec3adcd020a82ed127e2d843f74
857 programming <csdoig@mtu.edu>
2023-02-28 19:48:48 -0500

    game piece dependent intake / outtake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,21 +2,46 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IntakeSubsystem extends SubsystemBase{
+public class IntakeSubsystem extends SubsystemBase {
   TalonFX motor = new TalonFX(33);
-  public IntakeSubsystem() {
-  
+
+  public enum GamePiece {
+    Cone,
+    Cube
   }
 
+  private GamePiece m_GamePiece;
+
+  public IntakeSubsystem() {}
+
   @Override
-  public void periodic() {
-    
+  public void periodic() {}
+
+  public void setGamePiece(GamePiece piece) {
+    m_GamePiece = piece;
   }
 
   public void runIntake(double speed) {
-    motor.set(TalonFXControlMode.PercentOutput, speed);
+    if (m_GamePiece == GamePiece.Cube) {
+      if (speed > 0) {
+        motor.set(TalonFXControlMode.PercentOutput, 0.2 * speed);
+      } else {
+        motor.set(TalonFXControlMode.PercentOutput, 0.5 * speed);
+      }
+    } else {
+      motor.set(TalonFXControlMode.PercentOutput, -0.4 * speed);
+    }
+  }
+
+  public void intakeGamePiece(GamePiece piece) {
+    setGamePiece(piece);
+    runIntake(-1);
+  }
+
+  public void outtakeGamePiece(GamePiece piece) {
+    setGamePiece(piece);
+    runIntake(1);
   }
 }
