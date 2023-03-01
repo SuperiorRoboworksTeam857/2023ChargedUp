1b0b5880d5e2626c428ceb20eb1d6df31b52761f
857 programming <csdoig@mtu.edu>
2023-02-28 19:51:32 -0500

    formatting, some changes

--- src/main/java/frc/robot/autos/AutoPreloadChargeStation.java
+++ src/main/java/frc/robot/autos/AutoPreloadChargeStation.java
@@ -12,42 +12,43 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
 import frc.robot.Constants.AutoConstants;
+import frc.robot.RobotContainer;
 import frc.robot.commands.SelfBalanceCommand;
-import frc.robot.commands.TurnToAngleCommand;
-import frc.robot.subsystems.Swerve;
-import frc.robot.subsystems.ElevatorSubsystem.Positions;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.WristSubsystem;
 
 public class AutoPreloadChargeStation extends SequentialCommandGroup {
   public AutoPreloadChargeStation(RobotContainer robot) {
 
     PathPlannerTrajectory trajectory1 =
         PathPlanner.generatePath(
-            new PathConstraints(
-                1,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared),
+            new PathConstraints(1, AutoConstants.kMaxAccelerationMetersPerSecondSquared),
             new PathPoint(
-                new Translation2d(Units.inchesToMeters(0), Units.inchesToMeters(0)), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0)),
+                new Translation2d(Units.inchesToMeters(0), Units.inchesToMeters(0)),
+                Rotation2d.fromDegrees(0),
+                Rotation2d.fromDegrees(0)),
             new PathPoint(
-                new Translation2d(Units.inchesToMeters(160), Units.inchesToMeters(0)), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0)));
+                new Translation2d(Units.inchesToMeters(160), Units.inchesToMeters(0)),
+                Rotation2d.fromDegrees(0),
+                Rotation2d.fromDegrees(0)));
     PathPlannerTrajectory trajectory2 =
         PathPlanner.generatePath(
-            new PathConstraints(
-                1,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared),
+            new PathConstraints(1, AutoConstants.kMaxAccelerationMetersPerSecondSquared),
             new PathPoint(
-                new Translation2d(Units.inchesToMeters(160), Units.inchesToMeters(0)), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(0)),
+                new Translation2d(Units.inchesToMeters(160), Units.inchesToMeters(0)),
+                Rotation2d.fromDegrees(180),
+                Rotation2d.fromDegrees(0)),
             new PathPoint(
-                new Translation2d(Units.inchesToMeters(84), Units.inchesToMeters(0)), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(0)));
-    
+                new Translation2d(Units.inchesToMeters(84), Units.inchesToMeters(0)),
+                Rotation2d.fromDegrees(180),
+                Rotation2d.fromDegrees(0)));
+
     var thetaController =
         new ProfiledPIDController(
             Constants.AutoConstants.kPThetaController,
@@ -77,32 +78,28 @@ public class AutoPreloadChargeStation extends SequentialCommandGroup {
             robot.s_Swerve::setModuleStates,
             robot.s_Swerve);
 
-
     addCommands(
         new InstantCommand(
             () ->
                 robot.s_Swerve.resetOdometry(
                     new Pose2d(new Translation2d(0, 0), Rotation2d.fromDegrees(0)))),
-        
-        new InstantCommand(() -> robot.s_elevator.goToPosition(Positions.HIGH), robot.s_elevator),
+        new InstantCommand(
+            () -> robot.s_elevator.goToPosition(ElevatorSubsystem.Positions.HIGH),
+            robot.s_elevator),
         new WaitUntilCommand(robot.s_elevator::isElevatorAtGoal),
-        
+        new InstantCommand(
+            () -> robot.s_wrist.goToAngle(WristSubsystem.Positions.INTAKE), robot.s_elevator),
+        new WaitUntilCommand(robot.s_wrist::isWristAtGoal),
         new WaitCommand(1),
-
-        new InstantCommand(() -> robot.s_elevator.goToPosition(Positions.FLOOR), robot.s_elevator),
-        new WaitUntilCommand(robot.s_elevator::isElevatorAtGoal),
-
+        new InstantCommand(
+            () -> robot.s_wrist.goToAngle(WristSubsystem.Positions.VERTICAL), robot.s_elevator),
+        new InstantCommand(
+            () -> robot.s_elevator.goToPosition(ElevatorSubsystem.Positions.FLOOR),
+            robot.s_elevator),
         swerveControllerCommand1,
-
         new WaitCommand(0.5),
-
         swerveControllerCommand2,
-
-        new ParallelRaceGroup(
-            new SelfBalanceCommand(robot.s_Swerve),
-            new WaitCommand(3)
-        ),
-
+        new ParallelRaceGroup(new SelfBalanceCommand(robot.s_Swerve), new WaitCommand(3)),
         new InstantCommand(() -> robot.s_Swerve.drive(new Translation2d(0, 0), 0, false, true)));
   }
 }
