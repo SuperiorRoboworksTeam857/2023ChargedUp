88c135c670d6b5c73d1f7aed43b303c5c3624398
857 programming <csdoig@mtu.edu>
2023-03-14 19:56:21 -0400

    Make sure Limelight is in camera mode

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -58,6 +58,9 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+    m_robotContainer.m_limelight.turnOnDriverCam();
+    m_robotContainer.m_limelight.enableLimelight(false);
+    
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
@@ -76,6 +79,9 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
+    m_robotContainer.m_limelight.turnOnDriverCam();
+    m_robotContainer.m_limelight.enableLimelight(false);
+    
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
